b8a5f5a8ec070bed586c7f2f7572155157927a87
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-15 16:59:23 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 18, 'M', 'brown', 'shy',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy',2, 'M', 'black', 'playful',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 17, 'F', 'blue-gray', 'protective',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 24, 'M', 'brown', 'aggressive',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'white', 'shy',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 14, 'M', 'white', 'lazy',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 20, 'F', 'cinnamon', 'gentle',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'M', 'blue-gray', NULL,0);
